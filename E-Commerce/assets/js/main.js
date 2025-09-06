9cdcf55225b1bdee199bc415a3a768a4932287dc
Waqar <109896472+waqararshad786@users.noreply.github.com>
2025-09-06 13:20:31 +0000

    Add files via upload

---
+++ E-Commerce/assets/js/main.js
@@ -0,0 +1,56 @@
+// special countdwon 
+
+let dayItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
+
+let countDown = () => {
+    let futureDate = new Date("18 september 2025");
+    let currentDate = new Date();
+    let myDate = futureDate-currentDate;
+
+
+    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(myDate / 1000 / 60 / 60 ) % 24;
+    let min = Math.floor(myDate / 1000 / 60) % 60;
+    let sec = Math.floor(myDate / 1000 ) % 60;
+
+    dayItem.innerHTML = days;
+    hoursItem.innerHTML = hours;
+    minItem.innerHTML = min;
+    secItem.innerHTML = sec;
+}
+
+countDown();
+
+setInterval(countDown , 1000);
+
+
+//   scrolll 
+
+function scrollTopBack(){
+    let scrollTopButton = document.querySelector('#scrollup');
+    window.onscroll = function(){
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 250){
+            scrollTopButton.classList.add('scrollActive')
+        }
+        else{
+            scrollTopButton.classList.remove('scrollActive')
+        }
+    }
+}
+
+scrollTopBack();
+
+        // nav hide 
+
+ let navbar = document.querySelectorAll('.nav-link');
+ let navCollapse = document.querySelector('.navbar-collapse.collapse');
+ 
+ navbar.forEach(function(a){
+    a.addEventListener('click' , function(){
+        navCollapse.classList.remove('show');
+    })
+ })
